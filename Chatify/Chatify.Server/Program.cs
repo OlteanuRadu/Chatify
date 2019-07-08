e8f759d6da9b351532bbfc38cca187da47764d13
Olteanu Radu <radu.olteanu@vizolution.co.uk>
2019-07-08 15:32:31 +0300

    .

---
+++ Chatify/Chatify.Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+
+namespace Chatify.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ping 172.16.65.42
+            var url = "http://172.16.65.42:8080/";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine($"Server running at {url}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Chatify/Chatify.Server/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Owin;
+
+namespace Chatify.Server
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR("/signalchat", new HubConfiguration());
+
+            GlobalHost.Configuration.MaxIncomingWebSocketMessageSize = null;
+        }
+    }
+}
